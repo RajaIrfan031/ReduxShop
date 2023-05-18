b5e45a4c50f23fa25e2c0216e039594b9af5ec4e
rajairfan031 <irfankhan116752@gmail.com>
2023-05-18 12:06:13 +0100

    redux store setup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,23 @@
-import React from "react"; 
-import { Link } from 'react-router-dom';
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavBarPanel = ()=>{
+const NavBar = ()=>{
     return(
-    <div className="flex flex-row w-full h-16 bg-slate-300">
-      <div className="flex w-full justify-start">
-        <div className="ml-4 mt-2 flex flex-row">
-          <h1 className="text-4xl font-semibold">Shopity</h1>
-          <Link to="/" className="mt-3 ml-4 font-semibold bg-slate-500 rounded-md max-h-[32px] p-1"><h1>Products</h1></Link>
-        </div>
-      </div>
-      <div className="flex w-full justify-end">
-        <div className="mr-4 p-4 flex flex-row">
-        <Link to="/cart" className="font-semibold p-1 bg-slate-500 h-8 rounded-md max-h-[32px] w-full">
-          <h1>My Cart</h1>
-        </Link>
-        </div>
-      </div>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 }
 
-export default NavBarPanel;
+export default NavBar;
